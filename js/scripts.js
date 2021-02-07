f06ceae7be47c42723a048c2bd91a0930c3e32fc
Kang Gyeong Seok <rkdrudtjr159@gmail.com>
2021-02-07 22:27:35 +0900

    [Update] stack section chart update

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,94 @@
 (function () {
-    // navbar
-    if ($('.nav-link').length > 0) {
-      $('.nav-link, .navbar-brand').on('click', function() {
-        $('.nav-link.active').removeClass('active');
-        $(this).addClass('active');
-      });
-    }
-  
-    
-    let frondEndChart = document.querySelector('#chart_frondEnd')
-    let backEndChart = document.querySelector('#chart_backEnd')
-    let dataBaseChart = document.querySelector('#chart_database')
-    let utilChart = document.querySelector('#chart_util')
+  // navbar
+  if ($('.nav-link').length > 0) {
+    $('.nav-link, .navbar-brand').on('click', function () {
+      $('.nav-link.active').removeClass('active');
+      $(this).addClass('active');
+    });
+  }
 
-    if(frondEndChart){
-      var data = [{
-        values: [50, 40, 7, 3],
-        labels: ['JavaScript ES6', 'React', 'HTML5', 'CSS3'],
-        type: 'pie'
-      }];
-      
-      var layout = {
-        height: 400,
-        width: 500
-      };
-      
-      Plotly.newPlot(frondEndChart, data, layout);
-    }
-  
+
+  const frondEndChart = document.querySelector('#chart_frondEnd');
+  const backEndChart = document.querySelector('#chart_backEnd');
+  const dataBaseChart = document.querySelector('#chart_database');
+  const utilChart = document.querySelector('#chart_util');
+
+  const defaultLayout = {
+    paper_bgcolor: 'rgba(0,0,0,0)',
+    plot_bgcolor: 'rgba(0,0,0,0)',
+    margin: { t: 20, b: 20, r: 40, l: 40 },
+    showlegend: false,
+  }
+
+  if (frondEndChart) {
+    let data = [{
+      type: 'pie',
+      labels: ['JavaScript', 'TypeScript', 'React', 'HTML5', 'CSS'],
+      values: [30, 30, 20, 10, 10],
+      hole: 0.3,
+      textinfo: "label+percent",
+      hoverinfo: "label+percent",
+    }];
+    let layout = {
+      ...defaultLayout,
+      height: frondEndChart.height,
+      width: frondEndChart.width,
+    };
+
+    Plotly.newPlot(frondEndChart, data, layout, { displayModeBar: false, responsive: true });
+  }
+  if (backEndChart) {
+    let data = [{
+      type: 'pie',
+      labels: ['Node.js', 'Express', 'Java', 'Spring'],
+      values: [25, 25, 25, 25],
+      hole: 0.3,
+      textinfo: "label+percent",
+      hoverinfo: "label+percent",
+    }];
+
+    let layout = {
+      ...defaultLayout,
+      height: frondEndChart.height,
+      width: frondEndChart.width,
+    };
+    Plotly.newPlot(backEndChart, data, layout, { displayModeBar: false, responsive: true });
+  }
+  if (dataBaseChart) {
+    let data = [{
+      type: 'pie',
+      labels: ['OracleSQL', 'Tibero', 'MongoDB'],
+      values: [40, 40, 20],
+      hole: 0.3,
+      textinfo: "label+percent",
+      hoverinfo: "label+percent",
+    }];
+
+    let layout = {
+      ...defaultLayout,
+      height: frondEndChart.height,
+      width: frondEndChart.width,
+    };
+    Plotly.newPlot(dataBaseChart, data, layout, { displayModeBar: false, responsive: true });
+  }
+  if (utilChart) {
+    let data = [{
+      type: 'pie',
+      labels: ['GitHub', 'Jest', 'FireBase', 'Zeplin', 'etc'],
+      values: [20, 20, 20, 20, 20],
+      hole: 0.3,
+      textinfo: "label+percent",
+      hoverinfo: "label+percent",
+    }];
+
+    let layout = {
+      ...defaultLayout,
+      height: frondEndChart.height,
+      width: frondEndChart.width,
+    };
+    Plotly.newPlot(utilChart, data, layout, { displayModeBar: false, responsive: true });
+  }
+  /*
     if (chart1) {
       new Chart(chart1, {
         type: 'doughnut',
@@ -94,30 +155,29 @@
           }]
         }
       });
+    }*/
+
+  // carousel
+  if ($('.owl-carousel').length > 0) {
+    $('.owl-carousel').owlCarousel({
+      animateIn: 'fadeIn',
+      animateOut: 'fadeOut',
+      items: 1,
+      autoplay: false,
+      dots: true,
+      loop: true
+    });
+  }
+
+  // dark mode
+  if ($('.btn-toggle').length > 0) {
+    function switchMode(status) {
+      $('link[media]').attr('media', status ? 'screen' : 'not screen');
     }
-  
-    // carousel
-    if ($('.owl-carousel').length > 0) {
-      $('.owl-carousel').owlCarousel({
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        items: 1,
-        autoplay: false,
-        dots: true,
-        loop: true
-      });
-    }
-  
-    // dark mode
-    if ($('.btn-toggle').length > 0) {
-      function switchMode(status) {
-        $('link[media]').attr('media', status ? 'screen' : 'not screen');
-      }
-  
-      $('.btn-toggle').on('click', function() {
-        var useDarkMode = $(this).attr('data-use-dark-mode');
-        switchMode(useDarkMode === 'true');
-      });
-    }
-  })();
-  
+
+    $('.btn-toggle').on('click', function () {
+      var useDarkMode = $(this).attr('data-use-dark-mode');
+      switchMode(useDarkMode === 'true');
+    });
+  }
+})();
