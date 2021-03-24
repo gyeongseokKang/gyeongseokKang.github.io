43d50d6a76145830346a2edddf2306dc929b0dad
kyeungseok_kang <kyeungseok_kang@tmax.co.kr>
2021-03-24 14:55:25 +0900

    [Update] 연차별 업무 한줄 정리 추가, 리뉴얼 진행중 문구 삭제

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,35 @@
 (function () {
   // navbar
-  if ($('.nav-link').length > 0) {
-    $('.nav-link, .navbar-brand').on('click', function () {
-      $('.nav-link.active').removeClass('active');
-      $(this).addClass('active');
+  if ($(".nav-link").length > 0) {
+    $(".nav-link, .navbar-brand").on("click", function () {
+      $(".nav-link.active").removeClass("active");
+      $(this).addClass("active");
     });
   }
 
-
-  const frondEndChart = document.querySelector('#chart_frondEnd');
-  const backEndChart = document.querySelector('#chart_backEnd');
-  const dataBaseChart = document.querySelector('#chart_database');
-  const utilChart = document.querySelector('#chart_util');
+  const frondEndChart = document.querySelector("#chart_frondEnd");
+  const backEndChart = document.querySelector("#chart_backEnd");
+  const dataBaseChart = document.querySelector("#chart_database");
+  const utilChart = document.querySelector("#chart_util");
 
   const defaultLayout = {
-    paper_bgcolor: 'rgba(0,0,0,0)',
-    plot_bgcolor: 'rgba(0,0,0,0)',
+    paper_bgcolor: "rgba(0,0,0,0)",
+    plot_bgcolor: "rgba(0,0,0,0)",
     margin: { t: 20, b: 20, r: 40, l: 40 },
     showlegend: false,
-  }
+  };
 
   if (frondEndChart) {
-    let data = [{
-      type: 'pie',
-      labels: ['JavaScript', 'TypeScript', 'React', 'HTML5', 'CSS'],
-      values: [30, 30, 20, 10, 10],
-      hole: 0.3,
-      textinfo: "label+percent",
-      hoverinfo: "label+percent",
-    }];
+    let data = [
+      {
+        type: "pie",
+        labels: ["JavaScript", "TypeScript", "React", "HTML5", "CSS"],
+        values: [30, 30, 20, 10, 10],
+        hole: 0.3,
+        textinfo: "label+percent",
+        hoverinfo: "label+percent",
+      },
+    ];
     let layout = {
       ...defaultLayout,
       height: frondEndChart.height,
@@ -38,14 +39,16 @@
     Plotly.newPlot(frondEndChart, data, layout, { displayModeBar: false, responsive: true });
   }
   if (backEndChart) {
-    let data = [{
-      type: 'pie',
-      labels: ['Node.js', 'Express', 'Java', 'Spring'],
-      values: [25, 25, 25, 25],
-      hole: 0.3,
-      textinfo: "label+percent",
-      hoverinfo: "label+percent",
-    }];
+    let data = [
+      {
+        type: "pie",
+        labels: ["Node.js", "Express", "Java", "Spring"],
+        values: [25, 25, 25, 25],
+        hole: 0.3,
+        textinfo: "label+percent",
+        hoverinfo: "label+percent",
+      },
+    ];
 
     let layout = {
       ...defaultLayout,
@@ -55,14 +58,16 @@
     Plotly.newPlot(backEndChart, data, layout, { displayModeBar: false, responsive: true });
   }
   if (dataBaseChart) {
-    let data = [{
-      type: 'pie',
-      labels: ['OracleSQL', 'Tibero', 'MongoDB'],
-      values: [40, 40, 20],
-      hole: 0.3,
-      textinfo: "label+percent",
-      hoverinfo: "label+percent",
-    }];
+    let data = [
+      {
+        type: "pie",
+        labels: ["OracleSQL", "Tibero", "MongoDB"],
+        values: [40, 40, 20],
+        hole: 0.3,
+        textinfo: "label+percent",
+        hoverinfo: "label+percent",
+      },
+    ];
 
     let layout = {
       ...defaultLayout,
@@ -72,14 +77,16 @@
     Plotly.newPlot(dataBaseChart, data, layout, { displayModeBar: false, responsive: true });
   }
   if (utilChart) {
-    let data = [{
-      type: 'pie',
-      labels: ['GitHub', 'Jest', 'FireBase', 'Zeplin', 'etc'],
-      values: [20, 20, 20, 20, 20],
-      hole: 0.3,
-      textinfo: "label+percent",
-      hoverinfo: "label+percent",
-    }];
+    let data = [
+      {
+        type: "pie",
+        labels: ["GitHub", "Jest", "FireBase", "Zeplin", "etc"],
+        values: [20, 20, 20, 20, 20],
+        hole: 0.3,
+        textinfo: "label+percent",
+        hoverinfo: "label+percent",
+      },
+    ];
 
     let layout = {
       ...defaultLayout,
@@ -158,26 +165,26 @@
     }*/
 
   // carousel
-  if ($('.owl-carousel').length > 0) {
-    $('.owl-carousel').owlCarousel({
-      animateIn: 'fadeIn',
-      animateOut: 'fadeOut',
+  if ($(".owl-carousel").length > 0) {
+    $(".owl-carousel").owlCarousel({
+      animateIn: "fadeIn",
+      animateOut: "fadeOut",
       items: 1,
       autoplay: false,
       dots: true,
-      loop: true
+      loop: true,
     });
   }
 
   // dark mode
-  if ($('.btn-toggle').length > 0) {
+  if ($(".btn-toggle").length > 0) {
     function switchMode(status) {
-      $('link[media]').attr('media', status ? 'screen' : 'not screen');
+      $("link[media]").attr("media", status ? "screen" : "not screen");
     }
 
-    $('.btn-toggle').on('click', function () {
-      var useDarkMode = $(this).attr('data-use-dark-mode');
-      switchMode(useDarkMode === 'true');
+    $(".btn-toggle").on("click", function () {
+      var useDarkMode = $(this).attr("data-use-dark-mode");
+      switchMode(useDarkMode === "true");
     });
   }
 })();
