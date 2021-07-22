a339063f7d60b50ac87b128eb880b2b23c20df5e
gyeongseokKang <kyeungseok_kang@tmax.co.kr>
2021-07-22 13:29:05 +0900

    [Refactor] 스크롤에 따라 fade in하는 스타일 - aos 라이브러리로 변경

--- js/scripts.js
+++ js/scripts.js
@@ -115,45 +115,10 @@
     }
   });
 
-  $(window).on("load", function () {
-    function fade() {
-      let animation_height = $(window).innerHeight() * 0.5;
-      let ratio = Math.round((1 / animation_height) * 10000) / 10000;
-      $(".fade").each(function () {
-        let objectTop = $(this).offset().top;
-        let windowBottom = $(window).scrollTop() + $(window).innerHeight();
-        if (objectTop < windowBottom) {
-          if (objectTop < windowBottom - animation_height) {
-            $(this).css({
-              transition: "opacity 0.1s linear",
-              transition: "left 0.1s linear",
-              opacity: 1,
-              left: "0px",
-            });
-          } else {
-            $(this).css({
-              transition: "opacity 0.5s linear",
-              opacity: (windowBottom - objectTop) * ratio,
-              transition: "left 0.5s linear",
-              left: `${200 * (1 - (windowBottom - objectTop) * ratio)}px`,
-            });
-          }
-        } else {
-          $(this).css({
-            opacity: 0,
-            left: "200px",
-          });
-        }
-      });
-    }
-    $(".fade").css({
-      opacity: 0,
-      left: "200px",
-    });
-    fade();
-
-    $(window).scroll(function () {
-      fade();
-    });
+  //스크롤 연동 fade-in fade-out
+  AOS.init({
+    anchorPlacement: "top-bottom",
+    easing: "ease-in-out",
+    mirror: true,
   });
 })();
