53d4ce65d7007def90b0b19c78c3457e67070f92
gyeongseokKang <kyeungseok_kang@tmax.co.kr>
2021-07-26 15:03:11 +0900

    [New feature] scroll fade in 기능 변경
    - aos 라이브러리 -> intersection observer api로 변경
    - link hover css 수정 적용

--- js/scripts.js
+++ js/scripts.js
@@ -116,9 +116,20 @@
   });
 
   //스크롤 연동 fade-in fade-out
-  AOS.init({
-    anchorPlacement: "top-bottom",
-    easing: "ease-in-out",
-    mirror: true,
+  const targets = document.querySelectorAll(".fade-class");
+  const options = { root: null, threshold: 0.1, rootMargin: "-0px" };
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      const container = entry.target;
+      if (entry.isIntersecting) {
+        container.classList.add("fade-in");
+      } else {
+        container.classList.remove("fade-in");
+      }
+    });
+  }, options);
+
+  targets.forEach((target) => {
+    observer.observe(target);
   });
 })();
